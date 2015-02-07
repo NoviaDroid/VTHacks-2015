87ef64d86a9cd742228457d654806c23fcf963b9
Daniel <dan_pc@live.com>
2015-02-07 04:04:21 -0500

    Now using an extend viewport

--- core/src/com/dpc/vthacks/GameCamera.java
+++ core/src/com/dpc/vthacks/GameCamera.java
@@ -1,16 +1,22 @@
 package com.dpc.vthacks;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.dpc.vthacks.data.AppData;
 
 public class GameCamera extends OrthographicCamera {
+    private Viewport viewport;
     
     public GameCamera() {
+        viewport = new ExtendViewport(480, 800, this);
         
+        setToOrtho(false, AppData.width, AppData.height);
+        position.set(AppData.width * 0.5f, AppData.height * 0.5f, 0);
+        update();
     }
     
     public void resize(float width, float height) {
-        setToOrtho(false, width, height);
-        position.set(width * 0.5f, height * 0.5f, 0);
-        update();
+        viewport.update((int) width, (int) height);
     }
 }
