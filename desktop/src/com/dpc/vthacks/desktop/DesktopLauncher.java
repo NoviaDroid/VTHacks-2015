12b290767b128db7f184406e7fc96ff1780cee1a
Daniel <dan_pc@live.com>
2015-02-06 22:24:12 -0500

    Added project files

---
+++ desktop/src/com/dpc/vthacks/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dpc.vthacks.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dpc.vthacks.App;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new App(), config);
+	}
+}
